1a5eb5a878037292bc8b3eb7f3d6006c9aaf6985
Mahamat Naim Abdelkerim <73735806+NaimCode@users.noreply.github.com>
2021-11-06 02:11:38 +0100

    on my way

--- routes/getPaymentIntent.js
+++ routes/getPaymentIntent.js
@@ -1,15 +1,19 @@
 const router = require("express").Router();
 
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const stripe = require("stripe")(
+  "sk_test_51JoXgfFxlWbadRCPjDvc1ljK1AkKUlTmn4K40zfeVGryIBP4rXhT1Rsa6w6lRPKfqzW49XPPxfvGR5NGOj2W2Agp004NCvyaA1"
+);
 // Create a PaymentIntent with the order amount and currency
 
 router.post("/", async (req, res) => {
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: req.body.prix, // Specify amount here
-      currency: "eur", // Specify currency here
+      currency: "eur",
+      // Specify currency here
     });
     // Return client secret
+    console.log(paymentIntent);
     res.status(200).json({
       clientSecret: paymentIntent.client_secret,
     });
